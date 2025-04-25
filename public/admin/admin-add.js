1f17203c7ba95d85015a634fcdf9f6dc50ae44f7
ARTMashimbyi <2562071@students.wits.ac.za>
2025-04-25 23:46:08 +0200

    test

--- public/admin/admin-add.js
+++ public/admin/admin-add.js
@@ -1,12 +1,14 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-app.js";
-      import { 
-        getFirestore, 
-        collection, 
-        addDoc, 
-        getDocs, 
-        query, 
-        where
-      } from "https://www.gstatic.com/firebasejs/11.6.0/firebase-firestore.js";
+import { initializeApp } from "firebase/app";
+
+import {
+  getFirestore,
+  collection,
+  addDoc,
+  getDocs,
+  query,
+  where
+} from "firebase/firestore";
+
 
       const firebaseConfig = {
         apiKey: "AIzaSyAU_w_Oxi6noX_A1Ma4XZDfpIY-jkoPN-c",
