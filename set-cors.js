61713f1885c08f7861fe54dd663922eb7e952513
faranani-rich <2609702@students.wits.ac.za>
2025-05-17 23:29:16 +0200

    chore: rotate service-account key placeholder

--- set-cors.js
+++ set-cors.js
@@ -3,7 +3,7 @@ const { Storage } = require('@google-cloud/storage');
 const corsConfig  = require('./cors.json');
 
 async function main() {
-  const keyPath = path.resolve(__dirname, 'key', 'constitutionvault-1b5d1-firebase-adminsdk-fbsvc-e1edbb46b4.json');
+  const keyPath = path.resolve(__dirname, 'key', 'constitutionvault-1b5d1-firebase-adminsdk-fbsvc-6a8ce536de.json');
   const storage = new Storage({ keyFilename: keyPath });
   const bucket  = storage.bucket('constitutionvault-1b5d1.firebasestorage.app');
 
