25cd64dcdc16078426a67870b1e11eb3de2c1691
ARTMashimbyi <2562071@students.wits.ac.za>
2025-05-21 21:31:30 +0200

    testing setup again

--- jest.polyfills.js
+++ jest.polyfills.js
@@ -1,22 +1,22 @@
 // Polyfill for TextEncoder/TextDecoder
 if (typeof TextEncoder === 'undefined') {
-    const { TextEncoder, TextDecoder } = require('util');
-    global.TextEncoder = TextEncoder;
-    global.TextDecoder = TextDecoder;
-  }
-  
-  // Other necessary polyfills
-  if (typeof window !== 'undefined') {
-    global.window = window;
-    global.document = window.document;
-    global.navigator = window.navigator;
-  }
-  
-  // Mock for window.matchMedia
-  window.matchMedia = window.matchMedia || function() {
-    return {
-      matches: false,
-      addListener: function() {},
-      removeListener: function() {}
-    };
+  const { TextEncoder, TextDecoder } = require('util');
+  global.TextEncoder = TextEncoder;
+  global.TextDecoder = TextDecoder;
+}
+
+// Other necessary polyfills
+if (typeof window !== 'undefined') {
+  global.window = window;
+  global.document = window.document;
+  global.navigator = window.navigator;
+}
+
+// Mock for window.matchMedia
+window.matchMedia = window.matchMedia || function() {
+  return {
+    matches: false,
+    addListener: function() {},
+    removeListener: function() {}
   };
+};
